0b8cca4a65fd3b6958d78c43763602e49b38d731
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 11:20:19 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -5,7 +5,6 @@ import random
 
 ListeDeMots = ['osekour','programmation','secourisme','naufrage','difficile','compliqué','aidezmoi','silvousplait','python','cestdur'] #Liste de mots de plus de 6 lettres
 random.shuffle(ListeDeMots)
-print(ListeDeMots[1])
 LettreRecherchee = input("Entrer une lettre : ")
 for i in range (0,len(ListeDeMots[1])):
     if ((ListeDeMots[1])[i]) == LettreRecherchee:
