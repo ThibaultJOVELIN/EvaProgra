9781ada4696d814c33782d776684f0dece8664d8
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 10:47:17 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -5,4 +5,4 @@ import random
 
 ListeDeMots = ['osekour','programmation','secourisme','naufrage','difficile','compliqué','aidezmoi','silvousplait','python','cestdur']
 random.shuffle(ListeDeMots)
-print(ListeDeMots[1]) #La liste tire un mot au hasard
+print(ListeDeMots[1]) #La liste tire un mot au hasard parmis ceux proposés
