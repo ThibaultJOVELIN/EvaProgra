473ab0082adb0ce2d6e0475e6b02294765080fe8
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 12:04:24 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -6,10 +6,10 @@ from colorama import Fore, Back, Style
 
 #Version 0
 
-ListeDeMots = ListeDeMots = ['oskour','batons','rideau','ballon','apport','lardon','beurre','prison','python','besoin'] #Mots de 6 lettres
+ListeDeMots = ListeDeMots = ['oskour','batons','rideau','ballon','apport','lardon','beurre','prison','python','besoin'] #Mots de 6 lettres.
 random.shuffle(ListeDeMots)
 LettreRecherchee = input("Entrer une lettre (en minuscule) : ")
-for i in range (0,len(ListeDeMots[1])):         #Je prends le premier mot de la liste qui a été mélangée
+for i in range (0,len(ListeDeMots[1])):         #Je prends le premier mot de la liste qui a été mélangée.
     if ((ListeDeMots[1])[i]) == LettreRecherchee:
         print("Cette lettre existe en position :")
         print(i + 1)
@@ -19,4 +19,6 @@ for i in range (0,len(ListeDeMots[1])):         #Je prends le premier mot de la 
 MotPropose = input("Proposer un mot de 6 lettres : ")
 for i in range (0,6):
     if ((ListeDeMots[1])[i]) == MotPropose[i]:
-        print (Fore.RED + (ListeDeMots[1])[i]) 
+        print (Fore.RED + (ListeDeMots[1])[i])  #Les lettres à la bonne place apparaisent en rouge, les autres n'apparaissent pas.
+        
+input()
