9367752357852e5861bda0ed2e8f00ef09fe9aa8
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 09:47:53 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -1,1 +1,9 @@
-OSEKOUR
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text', end=" ")
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+input()
