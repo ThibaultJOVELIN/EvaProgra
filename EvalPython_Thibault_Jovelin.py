607d720cfd999c0036aace3e4ad66e0bcb8183aa
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 11:32:43 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -1,12 +1,22 @@
-
 import random
+
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
 #Version 0
 
-ListeDeMots = ['osekour','programmation','secourisme','naufrage','difficile','compliqué','aidezmoi','silvousplait','python','cestdur'] #Liste de mots de plus de 6 lettres
+ListeDeMots = ['osekour','programmation','secourisme','naufrage','difficile','compliqué','aidezmoi','silvousplait','python','cestdur']
 random.shuffle(ListeDeMots)
-LettreRecherchee = input("Entrer une lettre : ")
+LettreRecherchee = input("Entrer une lettre (en minuscule) : ")
 for i in range (0,len(ListeDeMots[1])):
     if ((ListeDeMots[1])[i]) == LettreRecherchee:
         print("Cette lettre existe en position :")
         print(i + 1)
+
+#Version 1
+
+MotPropose = [input("Proposer un mot : ")]
+for i in range (0,len(ListeDeMots[1])):
+    if ((ListeDeMots[1])[i]) == MotPropose[i]:
+        print (Fore.RED + (ListeDeMots[1])[i]) 
