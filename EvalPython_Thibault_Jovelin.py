a7523929bbd1113fd2bec947f788980233552112
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 10:30:06 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -1,19 +1,8 @@
-from colorama import init
-init()
-from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text', end=" ")
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
-print('back to normal now')
-print(Fore.RED + 'Normalement, je suis rouge')
 
-#Version 1
-Mot = ['OSEKOUR']
+#Version 0
+Mot = ['O','S','E','K','O','U','R']
 LettreRecherchee = input("Entrer une lettre à chercher (en majuscule)")
 for i in range (0, len(Mot)):
-    if Mot[i] == LettreRecherchee:
-        print(Mot[i])
-    
- 
-input()
+    if (Mot[i]) == LettreRecherchee:
+        print("Il y a cette lettre à la position :")
+        print(i + 1)
