04983de05b96c35371be30a54eab02583b56d852
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 10:46:11 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -1,8 +1,8 @@
 
+import random
+
 #Version 0
-Mot = ['O','S','E','K','O','U','R']
-LettreRecherchee = input("Entrer une lettre à chercher (en majuscule)")
-for i in range (0, len(Mot)):
-    if (Mot[i]) == LettreRecherchee:
-        print("Il y a cette lettre à la position :")
-        print(i + 1)
+
+ListeDeMots = ['osekour','programmation','secourisme','naufrage','difficile','compliqué','aidezmoi','silvousplait','python','cestdur']
+random.shuffle(ListeDeMots)
+print(ListeDeMots[1])
