facac0f245435fb5cdea780ebca9c8552f2be325
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2020-12-09 12:38:09 +0100

    Update EvalPython_Thibault_Jovelin.py

--- EvalPython_Thibault_Jovelin.py
+++ EvalPython_Thibault_Jovelin.py
@@ -25,7 +25,7 @@ for i in range (0,6):
         print (Fore.YELLOW + (ListeDeMots[1])[i]) #Maintenant, les lettres qui ne correspondent pas sont en jaune.
     
     elif MotPropose[i] == ((ListeDeMots[1])[i + 1]) or ((ListeDeMots[1])[i + 2]) or ((ListeDeMots[1])[i + 3]) or ((ListeDeMots[1])[i + 4]) or ((ListeDeMots[1])[i + 5]):
-        print (Fore.BLUE + (ListeDeMots[1])[i]) #Les lettres qui ne correspondent pas apparaissent en bleu (mais là ça marceh pas)
+        print (Fore.BLUE + (ListeDeMots[1])[i]) #Les lettres qui ne correspondent pas apparaissent en bleu (mais là ça marche pas)
     
     
 input()
